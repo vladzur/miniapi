c7bb2d9f122a2bc98abd2a4adbbbd0caeadbf236
vladzur <vladzur@gmail.com>
2017-03-20 09:44:28 -0300

    Using Fractal for data transformation output

--- app/Controller/BooksController.php
+++ app/Controller/BooksController.php
@@ -2,7 +2,9 @@
 namespace vladzur\miniapi\Controller;
 
 use vladzur\miniapi\Model\Book;
-
+use vladzur\miniapi\Transformer\BookTransformer;
+use League\Fractal;
+use League\Fractal\Manager;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
@@ -12,8 +14,10 @@ class BooksController
     public function index(Request $request, Response $response)
     {
         $books = Book::all();
+        $fractal = new Manager();
+        $resource = new Fractal\Resource\Collection($books, new BookTransformer);
         $response->withHeader('Content-Type', 'text/plain');
-        $response->getBody()->write(json_encode($books));
+        $response->getBody()->write($fractal->createData($resource)->toJson());
         return $response;
     }
 
@@ -24,8 +28,10 @@ class BooksController
         $book->author = $_REQUEST['author'];
         $book->ISBN = $_REQUEST['ISBN'];
         $book->save();
+        $fractal = new Manager();
+        $resource = new Fractal\Resource\Item($book, new BookTransformer);
         $response->withHeader('Content-Type', 'text/plain');
-        $response->getBody()->write(json_encode($book));
+        $response->getBody()->write($fractal->createData($resource)->toJson());
         return $response;
     }
 }
---
+++ app/Transformer/BookTransformer.php
@@ -0,0 +1,22 @@
+<?php
+namespace vladzur\miniapi\Transformer;
+
+use vladzur\miniapi\Model\Book;
+use League\Fractal;
+
+class BookTransformer extends Fractal\TransformerAbstract
+{
+	public function transform(Book $book)
+	{
+	    return [
+	        'id'      => (int) $book->id,
+	        'title'   => $book->title,
+	        'ISBN'    => $book->ISBN,
+            'author'   => [
+                [                    
+                    'name' => $book->author,
+                ]
+            ],
+	    ];
+	}
+}
